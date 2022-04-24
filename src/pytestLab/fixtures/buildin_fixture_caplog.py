71aede532067ecd851a0253b072dfc72010f457e
MS <mstuebner@gmail.com>
2022-04-24 17:37:32 +0200

    Clean code and add comments

--- src/pytestLab/fixtures/buildin_fixture_caplog.py
+++ src/pytestLab/fixtures/buildin_fixture_caplog.py
@@ -1,31 +1,16 @@
-import pytest
 import logging
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('buildin_fixture_caplog')
 
 
-@pytest.fixture(scope='function')
-def db():
-    print('*****SETUP DB*****')
-    yield db
-    print('******TEARDOWN DB******')
-
-
-@pytest.fixture(scope='function')
-def source():
-    print('*****SETUP SOURCE*****')
-    yield source
-    print('******TEARDOWN SOURCE******')
-
-
-def test_fixture_caplog_default(db, source, caplog):
+def test_fixture_caplog_default(caplog):
     log.info('This log will not show up in caplog')
     log.warning('warning log')
     print('test_fixture_caplog_default')  # Set breakpoint here to inspect caplog instance
 
 
-def test_fixture_caplog_setlevel(db, source, caplog):
+def test_fixture_caplog_setlevel(caplog):
     caplog.set_level(logging.INFO)
     log.info('Now also this INFO log will not show up in caplog')
     log.warning('warning log')
